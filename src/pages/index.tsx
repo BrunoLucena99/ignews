d9b9431a7509ba00ec85612e2a646fe764371286
BrunoLucena99 <brunohenriquelucena@gmail.com>
2021-11-11 20:26:09 -0300

    feat: start project with typescript and sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/home.module.scss';
+import { PageTitle } from '../components/PageTitle';
+
+export default function Home() {
+  return (
+    <>
+      <PageTitle title="Início | ig.news" />
+      <h1>
+        Hello World
+      </h1>
+    </>
+  )
+}
