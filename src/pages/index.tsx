5e92b3b563398eb4a4a6442885b5e8a6f4268c88
BrunoLucena99 <brunohenriquelucena@gmail.com>
2021-11-12 18:55:50 -0300

    feat: home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import styles from '../styles/home.module.scss';
 import { PageTitle } from '../components/PageTitle';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
-      <PageTitle title="Início | ig.news" />
-      <h1>
-        Hello World
-      </h1>
+      <PageTitle title="Home | ig.news" />
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.98 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
