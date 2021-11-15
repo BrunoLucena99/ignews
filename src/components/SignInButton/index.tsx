a0c60da0048456a6b9db808a4fb36e42d5455619
BrunoLucena99 <brunohenriquelucena@gmail.com>
2021-11-15 11:50:54 -0300

    feat: auth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  if (!isUserLoggedIn) {
+  const handleSignIn = () => signIn('github');
+  const handleSignOut = () => signOut();
+
+  if (!session) {
     return (
       <button
         type="button"
         className={styles.signInButton}
+        onClick={handleSignIn}
       >
         <FaGithub color="#eba417" />
         Sign in with Github
@@ -22,6 +27,7 @@ export function SignInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={handleSignOut}
     >
       <FaGithub color="#04b361" />
       Bruno Henrique
